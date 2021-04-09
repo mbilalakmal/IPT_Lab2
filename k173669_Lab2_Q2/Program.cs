87307798953cfd8f104c7e3c817ff93891d83b9e
mbilalakmal <37498621+mbilalakmal@users.noreply.github.com>
2021-04-09 22:37:16 +0500

    Creating 1 mil random ints

---
+++ k173669_Lab2_Q2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace k173669_Lab2_Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 1000000;
+            int min = 0;
+            int max = 1000;
+
+            Random randomGen = new Random();
+            List<int> numbers = Enumerable.Repeat(0, length).
+                Select(idx => randomGen.Next(min, max)).ToList();
+
+            Console.WriteLine(numbers.Count);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
