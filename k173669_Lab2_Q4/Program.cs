4d55ae22bc4b94aea7a7f25010749ee55163d802
mbilalakmal <37498621+mbilalakmal@users.noreply.github.com>
2021-04-09 23:44:47 +0500

    Added Q4

---
+++ k173669_Lab2_Q4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace k173669_Lab2_Q4
+{
+    class Program
+    {
+        const string URL = "https://www.bing.com/";
+        static void Main(string[] args)
+        {
+            Task task = new Task(FetchWebPageAsync);
+
+            task.Start();
+
+            /// Press Any Key - Main Thread is blocked, so the task can print to console
+            Console.ReadKey(true);
+            Console.WriteLine("Task Is Running");
+        }
+
+        public static async void FetchWebPageAsync()
+        {
+            HttpClient client = new();
+
+            try
+            {
+                var response = await client.GetAsync(URL);
+                var content = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(content);
+            }
+            catch (Exception exception) { Console.WriteLine(exception.ToString()); }
+
+            
+        }
+    }
+}
